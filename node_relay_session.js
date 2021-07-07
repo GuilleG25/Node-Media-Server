6fe8f500164366610d235c657c23fac90b2987d0
Your Name <you@example.com>
2021-07-07 02:00:51 +0000

    fidex

--- node_relay_session.js
+++ node_relay_session.js
@@ -21,7 +21,7 @@ class NodeRelaySession extends EventEmitter {
 
   run() {
     let format = this.conf.ouPath.startsWith('rtsp://') ? 'rtsp' : 'flv';
-    let argv = ['-re -i', this.conf.inPath, '-c', 'copy', '-f', format, this.conf.ouPath];
+    let argv = ['-re','-i', this.conf.inPath, '-c', 'copy', '-f', format, this.conf.ouPath];
     if (this.conf.inPath[0] === '/' || this.conf.inPath[1] === ':') {
       console.log(this.conf.inPath, this.conf.inPath[0])
       argv.unshift('-1');

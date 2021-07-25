707b1421ca392c22d27b3851605a915ed0ef4506
Your Name <you@example.com>
2021-07-25 20:23:02 +0000

    fix

--- node_trans_session.js
+++ node_trans_session.js
@@ -67,6 +67,7 @@ class NodeTransSession extends EventEmitter {
     Array.prototype.push.apply(argv, this.conf.acParam);
     Array.prototype.push.apply(argv, ['-f', 'tee', '-map', '0:a?', '-map', '0:v?', mapStr]);
     argv = argv.filter((n) => { return n }); //去空
+    console.log("hola",argv)
     this.ffmpeg_exec = spawn(this.conf.ffmpeg, argv);
     this.ffmpeg_exec.on('error', (e) => {
       Logger.ffdebug(e);
